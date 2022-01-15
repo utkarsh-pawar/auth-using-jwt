6901bb12e4d8efc04b2ba8f6fe5ea147fc73fd54
Utkarsh-pawar <utkarshpawar910910@gmail.com>
2022-01-15 19:39:01 +0530

    made some changes

--- frontend/src/components/ErrorNotice.js
+++ frontend/src/components/ErrorNotice.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const ErrorNotice = (props) => {
-    return (
-        <div className="error-notice">
-            <span>{props.message}</span>
-            <button onClick={props.clearError}>X</button>
-        </div>
-    )
-}
+  return (
+    <div className="error-notice">
+      <span>{props.message}</span>
+      <button onClick={props.clearError}>X</button>
+    </div>
+  );
+};
 
-export default ErrorNotice
+export default ErrorNotice;
